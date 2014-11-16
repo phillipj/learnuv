b9d381821ebc783d0b9bd2e0335becbe886d8001
Phillip Johnsen <johphi@gmail.com>
2014-11-16 15:59:48 +0100

    Exercise 4 (somewhat unstable)

--- src/04_fs_readasync.c
+++ src/04_fs_readasync.c
@@ -8,6 +8,8 @@ static uv_fs_t open_req;
 
 void read_cb(uv_fs_t* read_req) {
   int r = 0;
+  uv_loop_t *loop = uv_default_loop();
+
   if (read_req->result < 0) CHECK(read_req->result, "uv_fs_read callback");
 
   /* 4. Report the contents of the buffer */
@@ -16,6 +18,7 @@ void read_cb(uv_fs_t* read_req) {
 
   /* 5. Close the file descriptor */
   uv_fs_t close_req;
+  r = uv_fs_close(loop, &close_req, open_req.result, NULL);
   if (r < 0) CHECK(r, "uv_fs_close");
 
   uv_fs_req_cleanup(&open_req);
@@ -28,12 +31,16 @@ int main() {
   uv_loop_t *loop = uv_default_loop();
 
   /* 1. Open file (synchronously) */
+  r = uv_fs_open(loop, &open_req, filename, O_RDONLY, S_IRUSR, NULL);
   if (r < 0) CHECK(r, "uv_fs_open");
 
   /* 2. Create buffer and initialize it to turn it into a a uv_buf_t */
+  char buf[BUF_SIZE];
+  uv_buf_t uvbuffer = uv_buf_init(buf, sizeof(buf));
 
   /* 3. Use the file descriptor (the .result of the open_req) to read **aynchronously** from the file into the buffer */
   uv_fs_t read_req;
+  r = uv_fs_read(loop, &read_req, open_req.result, &uvbuffer, sizeof(buf), 0, read_cb);
   if (r < 0) CHECK(r, "uv_fs_read");
 
   uv_run(loop, UV_RUN_DEFAULT);
